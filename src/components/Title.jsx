cb45d942355bb717468a52e700bd962cf088fece
philip <popien.philip@gmail.com>
2023-07-16 20:25:07 +0200

    Update title styling for small screens

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 export default function Title({title}) {
   return (
     <header className="relative flex w-full">
-      <div className="text-red font-anom font-bold text-[46px] leading-[0.9em] tracking-tighter w-[150px] h-[80px] border-r-[2px] border-black">
+      <div className="absolute sm:relative w-full text-center sm:text-left text-red font-anom font-bold text-[46px] leading-[0.9em] tracking-tighter sm:w-[150px] h-[80px] sm:border-r-[2px] sm:border-black">
         <Link to="/about">Philip Popien</Link>
       </div>
-      <div className="relative -top-1  w-full overflow-hidden h-24">
+      <div className="relative top-8 mb-8 sm:mb-0 text-center sm:text-left sm:-top-1 w-full overflow-hidden h-24">
         <h1 id="header-title"
           className="relative text-black text-[100px] font-anom font-bold leading-[0.9em]"
-          style={{ letterSpacing: `-${title.length * 0.02}em` }}
+          style={{ letterSpacing: `-${title.length * 0.015}em` }}
         >
           {title}
         </h1>
